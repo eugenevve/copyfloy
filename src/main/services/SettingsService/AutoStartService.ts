917d377aaf63a023a5b80a783010fa4788abea94
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-08-05 16:26:09 +0600

    feat: a system for managing program autostart depending on the user's role for a given program

---
+++ src/main/services/SettingsService/AutoStartService.ts
@@ -0,0 +1,50 @@
+import { exec } from "child_process";
+
+import { app } from "electron";
+
+export class AutoStartService {
+  private readonly TASK_NAME = "BackupProgramAutoStart";
+
+  public update(openAtLogin: boolean, runAsAdmin: boolean): void {
+    if (!app.isPackaged || process.platform !== "win32") return;
+
+    const exePath = app.getPath("exe");
+
+    // Autostart via Task Scheduler (for administrators so that Windows does not block the program from launching)
+    if (openAtLogin && runAsAdmin) {
+      app.setLoginItemSettings({ openAtLogin: false });
+
+      const cmd =
+        `schtasks /create ` +
+        `/tn "${this.TASK_NAME}" ` +
+        `/tr "\\"${exePath}\\"" ` +
+        `/sc onlogon ` +
+        `/rl highest ` +
+        `/it ` +
+        `/f`;
+
+      exec(cmd);
+
+      return;
+    }
+
+    // Normal program startup (system registry)
+    if (openAtLogin) {
+      exec(`schtasks /delete /tn "${this.TASK_NAME}" /f`, () => {});
+
+      app.setLoginItemSettings({
+        openAtLogin: true,
+        path: exePath,
+      });
+
+      return;
+    }
+
+    // Disabled autostart
+    app.setLoginItemSettings({
+      openAtLogin: false,
+    });
+
+    exec(`schtasks /delete /tn "${this.TASK_NAME}" /f`, () => {});
+  }
+}
