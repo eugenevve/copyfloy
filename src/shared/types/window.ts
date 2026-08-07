6f1cad287e63e00d9464166d4a791cacb57d9a0e
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-08-07 12:19:41 +0600

    feat: adding settings typing

---
+++ src/shared/types/window.ts
@@ -0,0 +1,19 @@
+export interface IWindowState {
+  width: number;
+  height: number;
+  x?: number;
+  y?: number;
+  isMaximized: boolean;
+}
+
+export interface IAppSettings {
+  closeAction: SettingsAction.QUIT | SettingsAction.MINIMIZE;
+  autoStart: boolean;
+  runAsAdmin: boolean;
+  windowState?: IWindowState;
+}
+
+export enum SettingsAction {
+  QUIT = "quit",
+  MINIMIZE = "minimize",
+}
