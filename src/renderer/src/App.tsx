eb8a1b184db67bf36133ca812a998e382755ef38
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-08-27 00:10:33 +0600

    feat: declaring a settings page and using enum for page navigation

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,13 +3,16 @@ import { HashRouter, Route, Routes } from "react-router";
 
 import { GlobalWrapper } from "./components/GlobalWrapper";
 import { Home } from "./pages/Home";
+import { Settings } from "./pages/Settings";
+import { AppPath } from "./utils/router";
 
 export const App: FC = () => {
   return (
     <GlobalWrapper>
       <HashRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={AppPath.MAIN} element={<Home />} />
+          <Route path={AppPath.SETTINGS} element={<Settings />} />
         </Routes>
       </HashRouter>
     </GlobalWrapper>
