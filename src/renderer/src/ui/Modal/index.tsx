8dfd8e3e22fafbc30586b28c049c03306bb58485
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-09-05 18:29:21 +0600

    feat: adding a modal window UI component

---
+++ src/renderer/src/ui/Modal/index.tsx
@@ -0,0 +1,26 @@
+import { useEscapeKey } from "@app/hooks/useEscapeKey";
+import { FC } from "react";
+
+import styles from "./Modal.module.css";
+import { IModal } from "./Modal.types";
+import { Button } from "../Button";
+import { ButtonKind } from "../Button/Button.types";
+import { XmarkIcon } from "../Icons";
+
+export const Modal: FC<IModal> = ({ title, width, onClose, children }) => {
+  useEscapeKey(onClose);
+
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.container} style={{ width: width }}>
+        <div className={styles.header}>
+          <div className={styles.title}>{title}</div>
+          <Button kind={ButtonKind.SECONDARY} onClick={onClose} icon>
+            <XmarkIcon />
+          </Button>
+        </div>
+        <div className={styles.content}>{children}</div>
+      </div>
+    </div>
+  );
+};
