b92edc07f2d9fccec31d54fe623bb46f287c519c
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-09-05 18:21:24 +0600

    fix: corrected styling usage for the InputRadio label element.

--- src/renderer/src/ui/InputRadio/index.tsx
+++ src/renderer/src/ui/InputRadio/index.tsx
@@ -6,9 +6,9 @@ import { IInputRadio } from "./InputRadio.types";
 
 export const InputRadio: FC<IInputRadio> = ({ disabled, label, ...props }) => {
   return (
-    <label className={classNames(styles.container, (label && styles.label) || "", (disabled && styles.disabled) || "")}>
+    <label className={classNames(styles.container, (disabled && styles.disabled) || "")}>
       <input type="radio" {...props} disabled={disabled} className={styles.input} />
-      {label}
+      <div className={styles.label}>{label}</div>
     </label>
   );
 };
