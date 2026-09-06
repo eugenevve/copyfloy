10236a76e125a14bff9fb78002e1c41f47d958f3
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-09-07 00:19:14 +0600

    style: spelling correction

--- src/renderer/src/ui/NoData/index.tsx
+++ src/renderer/src/ui/NoData/index.tsx
@@ -4,5 +4,5 @@ import styles from "./NoData.module.css";
 import { INoData } from "./NoData.types";
 
 export const NoData: FC<INoData> = ({ title }) => {
-  return <div className={styles.container}>{title || "No data available"}</div>;
+  return <div className={styles.container}>{title || "No data available!"}</div>;
 };
