d2275ca83c6d2736ecb0b05cad5e80acb1f12490
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-09-11 00:41:20 +0600

    feat: adding elements to the settings page, including a new app update check component

--- src/renderer/src/pages/Settings/index.tsx
+++ src/renderer/src/pages/Settings/index.tsx
@@ -1,10 +1,15 @@
 import { PageWrapper } from "@app/components/PageWrapper";
 import { FC } from "react";
 
+import { UpdateChecker } from "./components/UpdateChecker";
+import styles from "./Settings.module.css";
+
 export const Settings: FC = () => {
   return (
     <PageWrapper>
-      Settings
+      <div className={styles.container}>
+        <UpdateChecker />
+      </div>
     </PageWrapper>
   );
 };
