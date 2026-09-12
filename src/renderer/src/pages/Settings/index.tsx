a1d4e9f439023403f97fd8491db6f607757964d5
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-09-12 12:53:49 +0600

    feat: implement ZoomController component with zoom in, zoom out, and reset functionality

--- src/renderer/src/pages/Settings/index.tsx
+++ src/renderer/src/pages/Settings/index.tsx
@@ -2,6 +2,7 @@ import { PageWrapper } from "@app/components/PageWrapper";
 import { FC } from "react";
 
 import { UpdateChecker } from "./components/UpdateChecker";
+import { ZoomController } from "./components/ZoomController";
 import styles from "./Settings.module.css";
 
 export const Settings: FC = () => {
@@ -9,6 +10,7 @@ export const Settings: FC = () => {
     <PageWrapper>
       <div className={styles.container}>
         <UpdateChecker />
+        <ZoomController />
       </div>
     </PageWrapper>
   );
