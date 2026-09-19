653c6b0a418f8fadc3be818b87f138d068b14818
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-09-19 21:14:58 +0600

    feat: implement Switch component with styles and types

---
+++ src/renderer/src/ui/Switch/Switch.types.ts
@@ -0,0 +1,5 @@
+import { InputHTMLAttributes } from "react";
+
+export interface ISwitch extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
---
+++ src/renderer/src/ui/Switch/index.tsx
@@ -0,0 +1,17 @@
+import { classNames } from "@app/utils/classNames";
+import { FC } from "react";
+
+import styles from "./Switch.module.css";
+import { ISwitch } from "./Switch.types";
+
+export const Switch: FC<ISwitch> = ({ disabled, label, ...props }) => {
+  return (
+    <label className={classNames(styles.container, (disabled && styles.disabled) || "")}>
+      <input type="checkbox" {...props} disabled={disabled} className={styles.input} />
+      <div className={styles.toggle}>
+        <div className={styles.thumb} />
+      </div>
+      <div className={styles.label}>{label}</div>
+    </label>
+  );
+};
